879f8d932d96081018fe1e46aca9f758068d112d
Atalay Görgün <atalaygrgn@gmail.com>
2025-05-02 16:52:44 +0300

    Revert "Added health, coin, and wave icons to gameplay screen"
    This reverts commit 719ad1a76ff4e00e90a6d34b70cf1729f6a38ad1.
    
    # Conflicts:
    #	src/main/resources/com/canja/kutowerdefence/gameplay-view.fxml

--- src/main/java/com/canja/kutowerdefence/ui/GamePlayView.java
+++ src/main/java/com/canja/kutowerdefence/ui/GamePlayView.java
@@ -11,8 +11,6 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.GridPane;
 import javafx.animation.AnimationTimer;
@@ -53,15 +51,6 @@ public class GamePlayView implements Initializable {
     @FXML
     private Button exitButton;
 
-    @FXML
-    private ImageView goldIcon;
-
-    @FXML
-    private ImageView healthIcon;
-
-    @FXML
-    private ImageView waveIcon;
-
     private GamePlayController controller;
 
     private final List<EnemyView> enemyViews = new ArrayList<>();
@@ -79,12 +68,7 @@ public class GamePlayView implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
         initializeButtons();
-        goldIcon.setImage(new Image("file:src/main/resources/assets/ui/button/0.png"));
-        healthIcon.setImage(new Image("file:src/main/resources/assets/ui/button/1.png"));
-        waveIcon.setImage(new Image("file:src/main/resources/assets/ui/button/2.png"));
-
     }
 
     private void initializeMapGridPane() {
@@ -97,6 +81,7 @@ public class GamePlayView implements Initializable {
                     int finalJ = j;
                     tileView.setOnMouseClicked(event -> {
                         controller.onEmptyLotClicked(tileView, finalI, finalJ);
+                        updateUI();
                     });
                 }
                 mapGridPane.add(tileView, i, j);
@@ -142,11 +127,12 @@ public class GamePlayView implements Initializable {
             view.update();
 
             if (view.isDead()) {
+                controller.rewardPlayer(enemy.getGoldReward());
                 toRemove.add(view);
             }
 
             if (enemy.reachedEnd()) {
-                controller.getPlayer().loseHealth();
+                controller.loseHealth();
                 toRemove.add(view);
             }
         }
