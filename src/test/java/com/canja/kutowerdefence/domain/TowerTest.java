07fd9cf8dd01ef8bf8fe04e1e794aecb709db341
Atalay Görgün <atalaygrgn@gmail.com>
2025-06-01 23:55:43 +0300

    Class tests for Tower

--- src/test/java/com/canja/kutowerdefence/domain/TowerTest.java
+++ src/test/java/com/canja/kutowerdefence/domain/TowerTest.java
@@ -51,21 +51,64 @@ class TowerTest {
 
     @Test
     void testTowerInitialization() {
-        // TODO
+        assertEquals(3, tower.range);
+        assertEquals(10, tower.damage);
+        assertEquals(100, tower.cost);
+        assertEquals(new Point(5, 5), tower.position);
+        assertEquals(MapObjectType.TOWER_ARCHER, tower.getType());
     }
 
     @Test
     void testEnemyInRange() {
-        // TODO
+        LinkedList<Point> path1 = new LinkedList<>();
+        path1.add(new Point(4, 4));
+        Enemy enemy1 = new Enemy(EnemyFactory.GOBLIN, path1);
+        assertTrue(tower.isInRange(enemy1), "Enemy should be in range");
+
+        LinkedList<Point> path2 = new LinkedList<>();
+        path2.add(new Point(9, 9));
+        Enemy enemy2 = new Enemy(EnemyFactory.GOBLIN, path2);
+        assertFalse(tower.isInRange(enemy2), "Enemy should be out of range");
     }
 
     @Test
     void testAttackRespectsCooldown() {
-        // TODO
+        LinkedList<Point> enemyPath = new LinkedList<>();
+        enemyPath.add(new Point(4, 4));
+        Enemy enemy = new Enemy(EnemyFactory.GOBLIN, enemyPath);
+        List<Enemy> enemies = new ArrayList<>();
+        enemies.add(enemy);
+
+        tower.tryAttack(enemies); // First attack
+        Enemy firstTarget = tower.getLastTarget();
+        assertNotNull(firstTarget, "Tower should have attacked");
+
+        tower.tryAttack(enemies); // Second attack attempt (should be blocked by cooldown)
+        assertEquals(firstTarget, tower.getLastTarget(),
+                "Tower should not have attacked again due to cooldown");
     }
 
     @Test
     void testTargetFurthestEnemy() {
-        // TODO
+        LinkedList<Point> path1 = new LinkedList<>();
+        path1.add(new Point(4, 4));
+        path1.add(new Point(5, 5));
+        Enemy enemy1 = new Enemy(EnemyFactory.GOBLIN, path1);
+
+        LinkedList<Point> path2 = new LinkedList<>();
+        path2.add(new Point(6, 6));
+        path2.add(new Point(7, 7));
+        path2.add(new Point(8, 8));
+        Enemy enemy2 = new Enemy(EnemyFactory.GOBLIN, path2);
+        // Update enemy2's position by simulating movement
+        enemy2.update(1.0f); // Move to make it further along its path
+
+        List<Enemy> enemies = new ArrayList<>();
+        enemies.add(enemy1);
+        enemies.add(enemy2);
+
+        Enemy[] targets = tower.targetEnemies(enemies);
+        assertEquals(1, targets.length, "Should target exactly one enemy");
+        assertEquals(enemy2, targets[0], "Should target the enemy further along the path");
     }
 }
